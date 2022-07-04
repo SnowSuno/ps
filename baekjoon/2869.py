61f07cd0be61dc5418f3b6faec024ca096ee6615
Soonho Kwon <percy3368@gmail.com>
2022-07-04 23:14:21 +0900

    Simplify baekjoon 2869

--- baekjoon/2869.py
+++ baekjoon/2869.py
@@ -1,10 +1,5 @@
 import math
 
-def calculateTime(increment, decrement, total):
-    return math.ceil((total - increment) / (increment - decrement)) + 1
+A, B, V = map(int, input().split())
 
-
-if __name__ == '__main__':
-    A, B, V = map(int, input().split())
-
-    print(calculateTime(A, B, V))
+print(math.ceil((V - A) / (A - B)) + 1)
